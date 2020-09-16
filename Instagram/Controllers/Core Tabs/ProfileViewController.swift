7b9345e052d8606a3144a8e1e85d3cbea71fe48f
Stanislav Kogiku <s.kogiku@gmail.com>
2020-09-16 11:50:46 +0900

    Rebuild Instagram App Tutorial by iOS.Academy. Status after video number 9.

--- Instagram/Controllers/Core Tabs/ProfileViewController.swift
+++ Instagram/Controllers/Core Tabs/ProfileViewController.swift
@@ -13,6 +13,8 @@ final class ProfileViewController: UIViewController {
 
     private var collectionView: UICollectionView?
     
+    private var userPosts = [UserPost]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -28,7 +30,7 @@ final class ProfileViewController: UIViewController {
         layout.itemSize = CGSize(width: size, height: size)
         collectionView = UICollectionView(frame: .zero,
                                           collectionViewLayout: layout)
-        collectionView?.backgroundColor = .red
+        
         
         // Cell
         collectionView?.register(PhotoCollectionViewCell.self, forCellWithReuseIdentifier: PhotoCollectionViewCell.identifier)
@@ -66,6 +68,7 @@ final class ProfileViewController: UIViewController {
 
 }
 
+// MARK: - Collection View
 extension ProfileViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     
     func numberOfSections(in collectionView: UICollectionView) -> Int {
@@ -76,17 +79,27 @@ extension ProfileViewController: UICollectionViewDelegate, UICollectionViewDataS
         if section == 0 {
             return 0
         }
+        // return userPosts.count
         return 30
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+//        let model = userPosts[indexPath.row]
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: PhotoCollectionViewCell.identifier, for: indexPath) as! PhotoCollectionViewCell
+//        cell.configure(with: model)
         cell.configure(debug: "test")
         return cell
     }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         collectionView.deselectItem(at: indexPath, animated: true)
+        
+        // get the model and open post controller
+//        let model = userPosts[indexPath.row]
+        let vc = PostViewController(model: nil)
+        vc.title = "Post"
+        vc.navigationItem.largeTitleDisplayMode = .never
+        navigationController?.pushViewController(vc, animated: true)
     }
     
     
@@ -108,6 +121,7 @@ extension ProfileViewController: UICollectionViewDelegate, UICollectionViewDataS
         let profileHeader = collectionView.dequeueReusableSupplementaryView(ofKind: kind,
                                                                      withReuseIdentifier: ProfileInfoHeaderCollectionReusableView.identifier,
                                                                      for: indexPath) as! ProfileInfoHeaderCollectionReusableView
+        profileHeader.delegate = self
         return profileHeader
     }
     
@@ -120,5 +134,35 @@ extension ProfileViewController: UICollectionViewDelegate, UICollectionViewDataS
         return CGSize(width: collectionView.width,
         height: 65)
     }
+    
+}
+
+// MARK: - ProfileInfoHeaderCollectionReusableViewDelegate
+extension ProfileViewController: ProfileInfoHeaderCollectionReusableViewDelegate {
+    func profileHeaderDidTapPostsButton(_ header: ProfileInfoHeaderCollectionReusableView) {
+        // scroll to the posts
+        collectionView?.scrollToItem(at: IndexPath(row: 0, section: 1), at: .top, animated: true)
+    }
+    
+    func profileHeaderDidTapFollowersButton(_ header: ProfileInfoHeaderCollectionReusableView) {
+        let vc = ListViewController()
+        vc.title = "Followers"
+        vc.navigationItem.largeTitleDisplayMode = .never
+        navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    func profileHeaderDidTapFollowingButton(_ header: ProfileInfoHeaderCollectionReusableView) {
+        let vc = ListViewController()
+        vc.title = "Followers"
+        vc.navigationItem.largeTitleDisplayMode = .never
+        navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    func profileHeaderDidTapEditProfileButton(_ header: ProfileInfoHeaderCollectionReusableView) {
+        let vc = EditProfileViewController()
+        vc.title = "Edit Profile"
+        present(UINavigationController(rootViewController: vc), animated: true)
+    }
+    
     
 }
--- Instagram/Controllers/Other/General/PostViewController.swift
+++ Instagram/Controllers/Other/General/PostViewController.swift
@@ -10,12 +10,56 @@ import UIKit
 
 class PostViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-       
+    private let model: UserPost?
+    
+    private let tableView: UITableView = {
+        let tableView = UITableView()
+        
+        return tableView
+    }()
+    
+    init(model: UserPost?) {
+        self.model = model
+        super.init(nibName: nil, bundle: nil)
     }
     
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.addSubview(tableView)
+        tableView.dataSource = self
+        tableView.delegate = self
+        view.backgroundColor = .systemBackground
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tableView.frame = view.bounds
+    }
 
 
+}
+
+extension PostViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        0
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
 }
